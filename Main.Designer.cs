782076fc8b81f7af47322f77905ed1c693462bed
WeGs <karimsamy11@hotmail.com>
2016-11-03 15:14:43 +0200

    Add files via upload

---
+++ Main.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace JMXbsrFile
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadbtn = new System.Windows.Forms.Button();
+            this.fFormat = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // loadbtn
+            // 
+            this.loadbtn.Location = new System.Drawing.Point(375, 489);
+            this.loadbtn.Name = "loadbtn";
+            this.loadbtn.Size = new System.Drawing.Size(75, 23);
+            this.loadbtn.TabIndex = 0;
+            this.loadbtn.Text = "Load";
+            this.loadbtn.UseVisualStyleBackColor = true;
+            this.loadbtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fFormat
+            // 
+            this.fFormat.Location = new System.Drawing.Point(213, 17);
+            this.fFormat.Name = "fFormat";
+            this.fFormat.ReadOnly = true;
+            this.fFormat.Size = new System.Drawing.Size(100, 20);
+            this.fFormat.TabIndex = 1;
+            this.fFormat.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(146, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "FileFormat:";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 55);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(438, 403);
+            this.treeView1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Thanks ->";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(56, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "DaxterSoul";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "for sharing file formats";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "credit goes ->";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DarkRed;
+            this.label6.Location = new System.Drawing.Point(81, 42);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "$Wegs";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(3, 464);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(161, 61);
+            this.panel1.TabIndex = 9;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 528);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fFormat);
+            this.Controls.Add(this.loadbtn);
+            this.Name = "Main";
+            this.Text = "Joymax bsr File (Read Only) | $Wegs";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadbtn;
+        private System.Windows.Forms.TextBox fFormat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace JMXbsrFile
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            treeView1.Visible = false;
+
+        }
+
+        public void teeview()
+        {
+            if (treeView1.Nodes.Count == 0)
+            {
+                treeView1.Visible = false;
+                treeView1.Nodes.Add("0", "");
+                treeView1.Nodes[0].Nodes.Add("0", "Materials");
+                treeView1.Nodes[0].Nodes.Add("1", "Meshes");
+                treeView1.Nodes[0].Nodes[1].Nodes.Add("0", "");
+                treeView1.Nodes[0].Nodes.Add("2", "Skeleton");
+                treeView1.Nodes[0].Nodes.Add("3", "Animations");
+            }
+            else
+            {
+                treeView1.Nodes.Clear();
+                treeView1.Visible = false;
+                treeView1.Nodes.Add("0", "");
+                treeView1.Nodes[0].Nodes.Add("0", "Materials");
+                treeView1.Nodes[0].Nodes.Add("1", "Meshes");
+                treeView1.Nodes[0].Nodes[1].Nodes.Add("0", "");
+                treeView1.Nodes[0].Nodes.Add("2", "Skeleton");
+                treeView1.Nodes[0].Nodes.Add("3", "Animations");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog bsrfile = new OpenFileDialog();
+                bsrfile.Filter = @"Joymax bsr Files (*.bsr) | *.bsr";
+                bsrfile.FilterIndex = 1;
+                bsrfile.Multiselect = false;
+                if (bsrfile.ShowDialog() == DialogResult.OK)
+                {
+                    teeview();
+                    string bsrpath = bsrfile.FileName;
+
+                    FileStream stream = new FileStream(bsrpath,FileMode.Open);
+                    using (BinaryReader read = new BinaryReader(stream))
+                    {
+                        char[] Format = read.ReadChars(12);
+                        fFormat.Text = ToString(Format);
+                      
+                        //pointers
+
+                        #region Pointers
+
+                        uint pMaterial = read.ReadUInt32();
+                        uint pMesh = read.ReadUInt32();
+                        uint pSkeleton = read.ReadUInt32();
+                        uint pAnimation = read.ReadUInt32();
+                        uint pMeshGroup = read.ReadUInt32();
+                        uint pAnimationGroup = read.ReadUInt32();
+                        uint pSoundEffect = read.ReadUInt32();
+                        uint pBoundingBox = read.ReadUInt32();
+
+                        #endregion
+                        //flags
+
+                        #region Flags
+
+                        uint funkUInt0 = read.ReadUInt32();
+                        uint funkUInt1 = read.ReadUInt32();
+                        uint funkUInt2 = read.ReadUInt32();
+                        uint funkUInt3 = read.ReadUInt32();
+                        uint funkUInt4 = read.ReadUInt32();
+
+                        #endregion
+                        //......
+                        string type = ToHex(read.ReadUInt32());
+                        int namelen = read.ReadInt32();
+                        char[] name = read.ReadChars(namelen);
+                        treeView1.Nodes[0].Text += ToString(name);  // bsr name
+                        byte[] unkBuffer = new byte[48];
+                        unkBuffer = read.ReadBytes(unkBuffer.Length);
+
+                        //-------Read Material Files--------\\
+
+                        #region Materials
+
+                        read.BaseStream.Position = pMaterial;
+                        uint materialcount = read.ReadUInt32();
+         
+                        for (int material = 0; materialcount > material; material++)
+                        {
+                            uint MaterialID = read.ReadUInt32();
+                            int MaterialPathLen = read.ReadInt32();
+                            char[] MaterialPath = read.ReadChars(MaterialPathLen);
+                            treeView1.Nodes[0].Nodes[0].Nodes.Add(Convert.ToString(materialcount), new string(MaterialPath));
+                        }
+
+                        #endregion
+                        //-------Read Mesh Files--------\\
+
+                        #region Meshes
+
+                        read.BaseStream.Position = pMesh;
+                        uint MeshCount = read.ReadUInt32();
+                        for (int meshindex = 0; MeshCount > meshindex; meshindex++)
+                        {
+                            int MeshNameLen = read.ReadInt32();
+                            char[] MeshName = read.ReadChars(MeshNameLen);
+                            if (funkUInt0 == 1)
+                            {
+                                uint meshUnkUInt0 = read.ReadUInt32();
+                            }
+                            treeView1.Nodes[0].Nodes[1].Nodes[0].Nodes.Add(Convert.ToString(meshindex), new string(MeshName));
+                        }
+
+                        #endregion
+
+                        //-------Read Animation Files--------\\
+
+                        #region Animation
+
+                        read.BaseStream.Position = pAnimation;
+                        uint unkUInt5 = read.ReadUInt32();
+                        uint unkUInt6 = read.ReadUInt32();
+                        uint animationCount = read.ReadUInt32();
+                        for (int animationIndex = 0; animationIndex < animationCount; animationIndex++)
+                        {
+                            int animationPathLen = read.ReadInt32();
+                            char[] animationPath = read.ReadChars(animationPathLen);
+                            treeView1.Nodes[0].Nodes[3].Nodes.Add(Convert.ToString(animationIndex),new string(animationPath));
+                        }
+
+                        #endregion
+
+                        //-------Read Skeleton Files--------\\
+
+                        #region Skeleton
+
+                        read.BaseStream.Position = pSkeleton;
+                        uint skeletonCount = read.ReadUInt32();
+                        for (int skeletonIndex = 0; skeletonIndex < skeletonCount; skeletonIndex++)
+                        {
+                            int skeletonPathLen = read.ReadInt32();
+                            char[] skeletonPath = read.ReadChars(skeletonPathLen);
+                            int extraByteCount = read.ReadInt32();
+                            byte[] skeletonExtraBytes = read.ReadBytes(extraByteCount);
+                            treeView1.Nodes[0].Nodes[2].Nodes.Add(Convert.ToString(skeletonIndex), new string(skeletonPath));
+                        }
+                        
+
+                        #endregion
+                        //-------Read MeshGroups--------\\
+
+                        #region MeshGroups
+
+                        read.BaseStream.Position = pMeshGroup;
+                        uint meshGroupCount = read.ReadUInt32();
+                        for (int i = 0; i < meshGroupCount; i++)
+                        {
+                            int meshGroupNameLen = read.ReadInt32();
+                            char[] meshGroupName = read.ReadChars(meshGroupNameLen);
+                            uint meshFileCount = read.ReadUInt32();
+                            
+                            treeView1.Nodes[0].Nodes[1].Nodes[0].Text = new string(meshGroupName);
+                            for (int ii = 0; ii < meshFileCount; ii++)
+                            {
+                                uint meshFileIndex = read.ReadUInt32();
+                            }
+                        }
+                        #endregion
+
+                        //-------Read Animation Groups--------\\
+
+                        #region ani groups
+
+                        /*
+                      
+                         //pointer.AnimationGroup will get you here
+4   uint    animationGroupCount
+for (int i = 0; i < animationGroupCount; i++)
+{       
+    4   uint    groupName.Length
+    4   uint    groupName
+
+    4   uint    animationCount
+    for (int ii = 0; ii < entryCount; ii++)
+    {
+        4   uint    animation.Type          //see ResourceAnimationType
+        4   uint    animation.FileIndex
+
+        4   uint    eventCount
+        for (int iii = 0; iii < eventCount; iii++)
+        {
+            4   uint    event.KeyTime
+            4   uint    event.Type
+            4   uint    event.unkValue0            
+            4   uint    event.unkValue1
+        }
+
+        4   uint    walkGraphPointCount
+        4   float   animation.WalkLength
+        for (int iiii = 0; iiii < eventCount; iiii++)
+        {
+            8   Vector2 walkGraphPoint
+        }
+    }
+}
+                         
+                         */
+                        #endregion
+
+                        treeView1.Visible = true;
+                        treeView1.ExpandAll();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            
+        }
+
+        public static string ToString(char[] chars)
+        {
+            return new string(chars);
+        }
+        public static string ToHex(uint value)
+        {
+            return String.Format("0x{0:X}", value);
+        }
+    }
+}
